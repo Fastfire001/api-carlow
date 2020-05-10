e0cfe7f0f4c00678e6c18ca7c146c42f6d2a417b
Maxime Maréchal <maxime.marechal@supinternet.fr>
2020-05-10 15:20:17 +0200

    Added events

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Fav;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+
+    /** @var Security */
+    private $security;
+
+    /** @var AuthorizationCheckerInterface */
+    private $auth;
+
+    public function __construct(Security $security, AuthorizationCheckerInterface $checker)
+    {
+        $this->security = $security;
+        $this->auth = $checker;
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    )
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        string $operationName = null,
+        array $context = []
+    )
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+        $user = $this->security->getUser();
+
+        if ($user instanceof User && !$this->auth->isGranted('ROLE_ADMIN')) {
+
+            if ($resourceClass === Fav::class) {
+                $rootAlias = $queryBuilder->getRootAliases()[0];
+                $queryBuilder->andWhere("$rootAlias.user = :user")
+                    ->setParameter('user', $user);
+            }
+        }
+    }
+}
